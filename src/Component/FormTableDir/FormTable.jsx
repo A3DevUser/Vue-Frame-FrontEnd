736e0c99a6e133a05420281789d69e1534c39fa1
A3DevUser <A3versioncontrol@gmail.com>
2023-11-22 11:54:21 +0530

    Hide column in react table

--- src/Component/FormTableDir/FormTable.jsx
+++ src/Component/FormTableDir/FormTable.jsx
@@ -198,9 +198,9 @@ const FormTable = ({col,dData,gridData,handleSave}) => {
           //   }
           // }, [dData]);
 
-          const initialState = { hiddenColumns : col.filter((fil)=>{return fil.hideShow=='true'}).map((res)=>{return res.accessor})}
+          const initialState = { hiddenColumns : col.filter((fil)=>{return fil.hideShow=='true' && gridData.gridId==fil.gridId }).map((res)=>{return res.accessor})}
 
-          console.log('initialState',initialState)
+          console.log(`initialState ${gridData.gridId}`,col.filter((fil)=>{return fil.hideShow=='true'}))
   
       const tableInstance = useTable({
           columns,
