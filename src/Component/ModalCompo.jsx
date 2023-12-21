9ff39fae354bedf204a6cd3b6dfb37d52561c7b5
Tejnarayan <a3devuser@gmail.com>
2023-12-21 11:41:44 +0530

    modul form css

--- src/Component/ModalCompo.jsx
+++ src/Component/ModalCompo.jsx
@@ -4,41 +4,41 @@ import { useDispatch, useSelector } from 'react-redux'
 import '../Component/CSS/ModalComp.css'
 import { LogInState } from '../Store/Actions/GeneralStates'
 
-export const ModalCompo = ({title,bodyDetails,show,showFunc}) => {
-  const NavBarRed = useSelector((state)=>state.NavBarRed)
-  const FormIdRed = useSelector((state)=>state.FormIdRed)
+export const ModalCompo = ({ title, bodyDetails, show, showFunc }) => {
+  const NavBarRed = useSelector((state) => state.NavBarRed)
+  const FormIdRed = useSelector((state) => state.FormIdRed)
 
   // console.log('modalcomp')
   return (
     <div>
       <Modal show={show} fullscreen={true} scrollable={true} onHide={showFunc}>
-        <Modal.Header style={{height:'8vh'}}>
-        <h3>{NavBarRed.val.filter((fil)=>{return fil.formId == FormIdRed})[0].navName}</h3>
-        <div className='modalHead'>
-        <button className="btn btn-primary btn-sm mx-2" onClick={showFunc}>Save</button>
-        <button className="btn btn-primary btn-sm" onClick={showFunc}>Close</button>
-        </div>
+        <Modal.Header style={{ height: '8vh' }}>
+            <h5 style={{ position: 'absolute', marginTop: '-3px' }} >{NavBarRed.val.filter((fil) => { return fil.formId == FormIdRed })[0].navName}</h5>
+          <div className='modalHead'>
+            <button className="btn btn-primary btn-sm mx-2" onClick={showFunc}>Save</button>
+            <button className="btn btn-primary btn-sm" onClick={showFunc}>Close</button>
+          </div>
         </Modal.Header>
-        <Modal.Body className='modalBody' style={{overflow:'hidden',maxHeight: 'calc(150vh - 150px)', overflowY: 'auto'}}>{bodyDetails}</Modal.Body>
-        <Modal.Footer style={{height:'50px'}}>
+        <Modal.Body className='modalBody' style={{ overflow: 'hidden', maxHeight: 'calc(150vh - 150px)', overflowY: 'auto' }}>{bodyDetails}</Modal.Body>
+        <Modal.Footer style={{ height: '50px' }}>
         </Modal.Footer>
       </Modal>
     </div>
   )
 }
 
-export const SimpleModalCompo = ({title,bodyDetails,show,showFunc}) => {
+export const SimpleModalCompo = ({ title, bodyDetails, show, showFunc }) => {
   const dispatch = useDispatch()
-  const UserDataStateRed = useSelector((state)=>state.UserDataStateRed)
+  const UserDataStateRed = useSelector((state) => state.UserDataStateRed)
 
-  const handleClick = ()=>{
+  const handleClick = () => {
     handleLogOut()
     showFunc()
   }
 
-  const handleLogOut = ()=>{
-        sessionStorage.removeItem('userData');
-        dispatch(LogInState(false))
+  const handleLogOut = () => {
+    sessionStorage.removeItem('userData');
+    dispatch(LogInState(false))
   }
   return (
     <div>
@@ -46,7 +46,7 @@ export const SimpleModalCompo = ({title,bodyDetails,show,showFunc}) => {
         <Modal.Header closeButton>{title}</Modal.Header>
         <Modal.Body><strong>Logged In User: </strong>{UserDataStateRed}</Modal.Body>
         <Modal.Footer>
-        <button className="btn btn-danger" onClick={handleClick}>Log out</button>
+          <button className="btn btn-danger" onClick={handleClick}>Log out</button>
         </Modal.Footer>
       </Modal>
     </div>
