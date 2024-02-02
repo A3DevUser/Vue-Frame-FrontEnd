1afe4991a4495b75a9c890867e7373f7e04fd969
A3DevUser <a3versioncontrol@gmail.com>
2024-02-02 15:04:17 +0530

    commit for pull

--- src/Component/EditViewAdd/AddTable.jsx
+++ src/Component/EditViewAdd/AddTable.jsx
@@ -123,7 +123,7 @@ const AddTable = () => {
           ColumnRed.val.filter((fil)=>{
             return fil.gridId == res.gridId
           }).forEach((fe)=>{return dataObj[fe.accessor]=''})
-         return  <AddTab columnData={ColumnRed.val.sort((a,b)=>{return a.number-b.number})} reportData={[]} gridData={res} handleSave={handleSave}  />
+         return  <AddTab columnData={ColumnRed.val.sort((a,b)=>{return a.orderNo-b.orderNo})} reportData={[]} gridData={res} handleSave={handleSave}  />
         //  <GridFormSub column={ColumnRed.val.sort((a,b)=>{return a.number-b.number})} data=
         //  {[]}
         //  gridData={res} key={i} handleSave={handleSave} disBtn={disBtn} setDisBtn={setDisBtn}/>
