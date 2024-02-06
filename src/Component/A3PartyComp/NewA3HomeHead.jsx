12c328ce46e08fdfacac7c3be4242730eedf62f9
Tejnarayan <a3devuser@gmail.com>
2024-02-06 12:13:07 +0530

    homesheet font size change

--- src/Component/A3PartyComp/NewA3HomeHead.jsx
+++ src/Component/A3PartyComp/NewA3HomeHead.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const NewA3HomeHead = ({getA3HomeData,getData,location}) => {
+const NewA3HomeHead = ({ getA3HomeData, getData, location }) => {
   return (
     <>
-    <div className='card card-body'>
-        <strong>Review Details<br/></strong>
-        Review Name : {location.state.review_name}
-    </div>
-    {/* <div className='card card-body vendorFilterDiv'>
+      <div className='card card-body'>
+        <strong style={{ fontSize: '18px' }} >Review Details<br /></strong>
+        <div style={{fontSize:'14px'}} > Review Name : {location.state.review_name} </div>
+      </div>
+      {/* <div className='card card-body vendorFilterDiv'>
         <label htmlFor="vendorFilter" id='vendorFilterLabel'>Vendor Type: </label>
         <select onChange={getA3HomeData} name="" id="vendorFilter" className='form-control vendorFilter'>
             <option value="">Select type</option>
